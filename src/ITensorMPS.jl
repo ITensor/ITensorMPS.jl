a6d882c59fbd1cd7ee38b51a57453dba6e9f93a4
mtfishman <mfishman@flatironinstitute.org>
2024-05-06 09:40:34 -0400

    Add more reexports

--- src/ITensorMPS.jl
+++ src/ITensorMPS.jl
@@ -1,8 +1,167 @@
 module ITensorMPS
-using ITensorTDVP: TimeDependentSum, dmrg_x, linsolve, tdvp, to_vec
-export TimeDependentSum, dmrg_x, linsolve, tdvp, to_vec
+using Reexport: @reexport
+@reexport using ITensorTDVP: TimeDependentSum, dmrg_x, linsolve, tdvp, to_vec
 using ITensorTDVP: ITensorTDVP
 const alternating_update_dmrg = ITensorTDVP.dmrg
-using ITensors.ITensorMPS: dmrg
-export dmrg
+@reexport using ITensors.ITensorMPS:
+  @OpName_str,
+  @SiteType_str,
+  @StateName_str,
+  @TagType_str,
+  @ValName_str,
+  @preserve_ortho,
+  @visualize,
+  @visualize!,
+  @visualize_noeval,
+  @visualize_noeval!,
+  @visualize_sequence,
+  @visualize_sequence_noeval,
+  AbstractMPS,
+  AbstractObserver,
+  Apply,
+  AutoMPO,
+  DMRGMeasurement,
+  DMRGObserver,
+  Lattice,
+  LatticeBond,
+  MPO,
+  MPS,
+  NoObserver,
+  Op,
+  OpName,
+  OpSum,
+  Ops,
+  Prod,
+  ProjMPO,
+  ProjMPOSum,
+  ProjMPO_MPS,
+  Scaled,
+  SiteType,
+  Spectrum,
+  StateName,
+  Sum,
+  Sweeps,
+  Trotter,
+  ValName,
+  add,
+  add!,
+  apply,
+  applyMPO,
+  applympo,
+  argsdict,
+  checkdone!, # remove export
+  coefficient,
+  common_siteind,
+  common_siteinds,
+  correlation_matrix,
+  cutoff,
+  cutoff!, # deprecate
+  disk,
+  dmrg,
+  dot, # remove export
+  eigs, # deprecate
+  energies, # deprecate
+  entropy, # deprecate
+  errorMPOprod, # deprecate
+  error_contract,
+  error_mpoprod, # deprecate
+  error_mul, # deprecate
+  expect,
+  findfirstsiteind, # deprecate
+  findfirstsiteinds, # deprecate
+  findsite, # deprecate
+  findsites, # deprecate
+  firstsiteind, # deprecate
+  firstsiteinds, # deprecate
+  get_cutoffs, # deprecate
+  get_maxdims, # deprecate
+  get_mindims, # deprecate
+  get_noises, # deprecate
+  has_fermion_string, # remove export
+  hassameinds,
+  inner,
+  isortho,
+  linkdim,
+  linkdims,
+  linkind,
+  linkindex,
+  linkinds,
+  logdot,
+  loginner,
+  lognorm,
+  lproj,
+  maxdim,
+  maxdim!,
+  maxlinkdim,
+  measure!,
+  measurements,
+  mindim,
+  mindim!,
+  movesite,
+  movesites,
+  mul, # deprecate
+  multMPO,
+  multmpo,
+  noise,
+  noise!,
+  noiseterm,
+  nsite,
+  nsweep,
+  op,
+  ops,
+  orthoCenter,
+  ortho_lims,
+  orthocenter,
+  orthogonalize,
+  orthogonalize!,
+  outer,
+  position!,
+  product,
+  primelinks!,
+  productMPS,
+  projector,
+  promote_itensor_eltype,
+  randomITensor,
+  randomMPO,
+  randomMPS,
+  replace_siteinds,
+  replace_siteinds!,
+  replacebond,
+  replacebond!,
+  replaceprime,
+  replacesites!,
+  reset_ortho_lims!,
+  rproj,
+  sample,
+  sample!,
+  set_leftlim!,
+  set_ortho_lims!,
+  set_rightlim!,
+  setcutoff!,
+  setmaxdim!,
+  setmindim!,
+  setnoise!,
+  simlinks!,
+  siteind,
+  siteindex,
+  siteinds,
+  splitblocks,
+  square_lattice,
+  state,
+  sum,
+  swapbondsites,
+  sweepnext,
+  tensors,
+  toMPO,
+  totalqn,
+  tr,
+  triangular_lattice,
+  truncate,
+  truncate!,
+  truncerror,
+  truncerrors,
+  unique_siteind,
+  unique_siteinds,
+  val,
+  ⋅
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,36 @@
 @eval module $(gensym())
 using ITensorMPS: ITensorMPS
 using ITensorTDVP: ITensorTDVP
+using ITensors: ITensors
+include(
+  joinpath(
+    pkgdir(ITensors),
+    "test",
+    "base",
+    "utils",
+    "TestITensorsExportedNames",
+    "TestITensorsExportedNames.jl",
+  ),
+)
 using Test: @test, @testset
 @testset "ITensorMPS.jl" begin
   @testset "exports" begin
     @test issetequal(
       names(ITensorMPS),
       [
-        :ITensorMPS,
-        # ITensorMPS reexports
-        :dmrg,
+        [
+          :ITensorMPS,
+        ]
         # ITensorTDVP reexports
-        :TimeDependentSum,
-        :dmrg_x,
-        :linsolve,
-        :tdvp,
-        :to_vec,
+        [
+          :TimeDependentSum,
+          :dmrg_x,
+          :linsolve,
+          :tdvp,
+          :to_vec,
+        ]
+        # ITensors.ITensorMPS reexports
+        TestITensorsExportedNames.ITENSORMPS_EXPORTED_NAMES
       ],
     )
   end
