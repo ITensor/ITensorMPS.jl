a118bd5075b2e00a256caa531d1bbd113886e0fd
Matt Fishman <mtfishman@users.noreply.github.com>
2024-05-06 11:40:53 -0400

    Reexport from ITensorTDVP and ITensors.ITensorMPS (#1)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,24 @@
-using ITensorMPS
-using Test
-
+@eval module $(gensym())
+using ITensorMPS: ITensorMPS
+using ITensorTDVP: ITensorTDVP
+using ITensors: ITensors
+include("utils/TestITensorMPSExportedNames.jl")
+using Test: @test, @testset
 @testset "ITensorMPS.jl" begin
-    # Write your tests here.
+  @testset "exports" begin
+    @test issetequal(
+      names(ITensorMPS),
+      [
+        [:ITensorMPS]
+        # ITensorTDVP reexports
+        [:TimeDependentSum, :dmrg_x, :linsolve, :tdvp, :to_vec]
+        # ITensors and ITensors.ITensorMPS reexports
+        TestITensorMPSExportedNames.ITENSORMPS_EXPORTED_NAMES
+      ],
+    )
+  end
+  @testset "aliases" begin
+    @test ITensorMPS.alternating_update_dmrg === ITensorTDVP.dmrg
+  end
+end
 end
