e64592098af56eb77e41134f832d673faba28658
Documenter.jl <documenter@juliadocs.github.io>
2024-05-17 01:32:06 +0000

    build based on 0362267

---
+++ v0.2.2/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ITensorMPS","category":"page"},{"location":"#ITensorMPS","page":"Home","title":"ITensorMPS","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ITensorMPS.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ITensorMPS]","category":"page"}]
+}
