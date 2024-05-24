f8f4dbdddf70bea9f0acea635a823558c9e0e0de
mtfishman <mfishman@flatironinstitute.org>
2024-05-24 12:07:24 -0400

    Add solver examples

---
+++ examples/04_tdvp_observers.jl
@@ -0,0 +1,45 @@
+using ITensorMPS: MPO, MPS, OpSum, expect, inner, siteinds, tdvp
+using Observers: observer
+
+function main()
+  function heisenberg(N)
+    os = OpSum()
+    for j in 1:(N - 1)
+      os += 0.5, "S+", j, "S-", j + 1
+      os += 0.5, "S-", j, "S+", j + 1
+      os += "Sz", j, "Sz", j + 1
+    end
+    return os
+  end
+
+  N = 10
+  s = siteinds("S=1/2", N; conserve_qns=true)
+  H = MPO(heisenberg(N), s)
+
+  step(; sweep) = sweep
+  current_time(; current_time) = current_time
+  return_state(; state) = state
+  measure_sz(; state) = expect(state, "Sz"; sites=length(state) ÷ 2)
+  obs = observer(
+    "steps" => step, "times" => current_time, "states" => return_state, "sz" => measure_sz
+  )
+
+  init = MPS(s, n -> isodd(n) ? "Up" : "Dn")
+  state = tdvp(
+    H, -1.0im, init; time_step=-0.1im, cutoff=1e-12, (step_observer!)=obs, outputlevel=1
+  )
+
+  println("\nResults")
+  println("=======")
+  for n in 1:length(obs.steps)
+    print("step = ", obs.steps[n])
+    print(", time = ", round(obs.times[n]; digits=3))
+    print(", |⟨ψⁿ|ψⁱ⟩| = ", round(abs(inner(obs.states[n], init)); digits=3))
+    print(", |⟨ψⁿ|ψᶠ⟩| = ", round(abs(inner(obs.states[n], state)); digits=3))
+    print(", ⟨Sᶻ⟩ = ", round(obs.sz[n]; digits=3))
+    println()
+  end
+  return nothing
+end
+
+main()
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,46 +1,14 @@
 @eval module $(gensym())
+using Test: @testset
 using ITensorMPS: ITensorMPS
-using ITensorTDVP: ITensorTDVP
-using ITensors: ITensors
-include("utils/TestITensorMPSExportedNames.jl")
-using Test: @test, @test_broken, @testset
+test_path = joinpath(pkgdir(ITensorMPS), "test")
+test_files = filter(
+  file -> startswith(file, "test_") && endswith(file, ".jl"), readdir(test_path)
+)
 @testset "ITensorMPS.jl" begin
-  @testset "exports" begin
-    @test issetequal(
-      names(ITensorMPS),
-      [
-        [:ITensorMPS]
-        # ITensorTDVP reexports
-        [:TimeDependentSum, :dmrg_x, :expand, :linsolve, :tdvp, :to_vec]
-        # ITensors and ITensors.ITensorMPS reexports
-        TestITensorMPSExportedNames.ITENSORMPS_EXPORTED_NAMES
-      ],
-    )
-  end
-  @testset "Aliases" begin
-    @test ITensorMPS.Experimental.dmrg === ITensorTDVP.dmrg
-    @test ITensorMPS.dmrg === ITensors.ITensorMPS.dmrg
-  end
-  @testset "Not exported" begin
-    @test ITensorMPS.sortmergeterms === ITensors.ITensorMPS.sortmergeterms
-    # Should we fix this in ITensors.jl by adding:
-    # ```julia
-    # using .ITensorMPS: sortmergeterms
-    # ```
-    # ?
-    @test_broken ITensorMPS.sortmergeterms === ITensors.sortmergeterms
-    for f in [
-      :AbstractProjMPO,
-      :AbstractMPS,
-      :ProjMPS,
-      :makeL!,
-      :makeR!,
-      :set_terms,
-      :sortmergeterms,
-      :terms,
-    ]
-      @test getfield(ITensorMPS, f) === getfield(ITensors.ITensorMPS, f)
-    end
+  @testset "$filename" for filename in test_files
+    println("Running $filename")
+    @time include(joinpath(test_path, filename))
   end
 end
 end
