51fc2e5c58aff50ff48574f021fe38b46530ebce
mtfishman <mfishman@flatironinstitute.org>
2024-10-22 14:46:58 -0400

    Absorb ITensors.ITensorMPS

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,13 @@
-@eval module $(gensym())
-using Test: @testset
-using ITensorMPS: ITensorMPS
-test_path = joinpath(pkgdir(ITensorMPS), "test")
-test_files = filter(
-  file -> startswith(file, "test_") && endswith(file, ".jl"), readdir(test_path)
-)
-@testset "ITensorMPS.jl" begin
-  @testset "$filename" for filename in test_files
-    println("Running $filename")
-    @time include(joinpath(test_path, filename))
+using ITensors
+using Test
+
+ITensors.Strided.disable_threads()
+ITensors.BLAS.set_num_threads(1)
+ITensors.disable_threaded_blocksparse()
+
+@testset "$(@__DIR__)" begin
+  dirs = ["ext/ITensorMPSChainRulesCoreExt", "Ops", "base"]
+  for dir in dirs
+    @time include(joinpath(@__DIR__, dir, "runtests.jl"))
   end
-end
 end
