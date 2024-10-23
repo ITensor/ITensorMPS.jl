8362255492b6d2714636416678a5731304614905
mtfishman <mfishman@flatironinstitute.org>
2024-10-23 11:00:23 -0400

    More test fixes

--- test/base/test_solvers/test_examples.jl
+++ test/base/test_solvers/test_examples.jl
@@ -1,12 +1,12 @@
 @eval module $(gensym())
-using Suppressor: @suppress
 using ITensorMPS: ITensorMPS
+using Suppressor: @suppress
 using Test: @testset
 @testset "Run examples" begin
   examples_files = [
     "01_tdvp.jl", "02_dmrg-x.jl", "03_tdvp_time_dependent.jl", "04_tdvp_observers.jl"
   ]
-  examples_path = joinpath(pkgdir(ITensorMPS), "examples")
+  examples_path = joinpath(pkgdir(ITensorMPS), "examples", "solvers")
   @testset "Running example file $f" for f in examples_files
     println("Running example file $f")
     @suppress include(joinpath(examples_path, f))
