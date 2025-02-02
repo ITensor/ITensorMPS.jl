d896610f10202d550026bf39742d3c786545dcb3
mtfishman <mfishman@flatironinstitute.org>
2025-02-02 08:43:52 -0500

    Simplified siteinds

--- src/siteinds.jl
+++ src/siteinds.jl
@@ -1,7 +1,7 @@
 using ITensors: ITensors, Index, settag
-using QuantumOperatorDefinitions: SiteType
+using QuantumOperatorDefinitions: sites
 
 # TODO: Move to `ITensorSites.jl`?
 function siteinds(t::String, n::Int; kwargs...)
-  return [settag(Index(SiteType(t; kwargs...)), "n", string(j)) for j in 1:n]
+  return sites(Index, t, n; kwargs...)
 end
