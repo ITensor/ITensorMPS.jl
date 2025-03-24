6bf437f1a6c51bb1b38f71c67ff4745d6ede4fbd
mtfishman <mfishman@flatironinstitute.org>
2025-03-24 10:35:31 -0400

    Add support section to docs

--- docs/make_index.jl
+++ docs/make_index.jl
@@ -1,9 +1,20 @@
 using Literate: Literate
 using ITensorMPS: ITensorMPS
 
+function ccq_logo(content)
+  include_ccq_logo = """
+    ```@raw html
+    <img src="assets/CCQ.png" width="20%" alt="Flatiron Center for Computational Quantum Physics logo.">
+    ```
+    """
+  content = replace(content, "{CCQ_LOGO}" => include_ccq_logo)
+  return content
+end
+
 Literate.markdown(
   joinpath(pkgdir(ITensorMPS), "examples", "README.jl"),
   joinpath(pkgdir(ITensorMPS), "docs", "src");
   flavor=Literate.DocumenterFlavor(),
   name="index",
+  postprocess=ccq_logo,
 )
