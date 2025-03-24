75775c846a655c69080a22c139015e8d44dd2673
Matt Fishman <mtfishman@users.noreply.github.com>
2025-03-24 11:25:06 -0400

    Add support section to docs (#128)

--- docs/make_readme.jl
+++ docs/make_readme.jl
@@ -1,9 +1,18 @@
 using Literate: Literate
 using ITensorMPS: ITensorMPS
 
+function ccq_logo(content)
+  include_ccq_logo = """
+    <img src="docs/src/assets/CCQ.png" width="20%" alt="Flatiron Center for Computational Quantum Physics logo.">
+    """
+  content = replace(content, "{CCQ_LOGO}" => include_ccq_logo)
+  return content
+end
+
 Literate.markdown(
   joinpath(pkgdir(ITensorMPS), "examples", "README.jl"),
   joinpath(pkgdir(ITensorMPS));
   flavor=Literate.CommonMarkFlavor(),
   name="README",
+  preprocess=ccq_logo,
 )
