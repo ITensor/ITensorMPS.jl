4e860ce4cffdb2fef5aec32ba8edfd194dfbf467
mtfishman <mfishman@flatironinstitute.org>
2025-03-26 17:35:14 -0400

    Update docs make

--- docs/make_readme.jl
+++ docs/make_readme.jl
@@ -3,7 +3,10 @@ using ITensorMPS: ITensorMPS
 
 function ccq_logo(content)
   include_ccq_logo = """
-    <img src="docs/src/assets/CCQ.png" width="20%" alt="Flatiron Center for Computational Quantum Physics logo.">
+    <picture>
+      <source media="(prefers-color-scheme: dark)" width="20%" srcset="docs/src/assets/CCQ-dark.png">
+      <img alt="Flatiron Center for Computational Quantum Physics logo." width="20%" src="docs/src/assets/CCQ.png">
+    </picture>
     """
   content = replace(content, "{CCQ_LOGO}" => include_ccq_logo)
   return content
