de79da1accec3662a88bf8fb27d5f1a687afe155
Vincenzo Bisogno <134813610+VinceNeede@users.noreply.github.com>
2025-05-01 01:54:44 +0200

    [DOCS] Update set_ortho_lims! docs

--- src/abstractmps.jl
+++ src/abstractmps.jl
@@ -127,11 +127,11 @@ function ortho_lims(ψ::AbstractMPS)
 end
 
 """
-    ITensors.set_ortho_lims!(::MPS/MPO, r::UnitRange{Int})
+    set_ortho_lims!(::MPS/MPO, r::UnitRange{Int})
 
 Sets the range of sites of the orthogonality center of the MPS/MPO.
 
-This is not exported and is an advanced feature that should be used with
+This is an advanced feature that should be used with
 care. Setting the orthogonality limits wrong can lead to incorrect results
 when using ITensor MPS/MPO functions.
 
