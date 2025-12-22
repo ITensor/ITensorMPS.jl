2e6a7432e28588f9b87621ee5e96030251c95f5c
sotashimozono <shimozono-sota631@g.ecc.u-tokyo.ac.jp>
2025-12-23 00:39:24 +0800

    fixed typo at solvers/linsolve.jl (#169)

--- src/solvers/linsolve.jl
+++ src/solvers/linsolve.jl
@@ -29,7 +29,7 @@ Keyword arguments:
   - `nsweeps`, `cutoff`, `maxdim`, etc. (like for other MPO/MPS updaters).
   - `updater_kwargs=(;)` - a `NamedTuple` containing keyword arguments that will get forwarded to the local updater,
     in this case `KrylovKit.linsolve` which is a GMRES linear updater. For example:
-    ```juli
+    ```julia
     linsolve(A, b, x; maxdim=100, cutoff=1e-8, nsweeps=10, updater_kwargs=(; ishermitian=true, tol=1e-6, maxiter=20, krylovdim=30))
     ```
     See `KrylovKit.jl` documentation for more details on available keyword arguments.
