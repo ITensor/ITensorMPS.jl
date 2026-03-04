e6a95245f8254954cfcee92924c83513a6c26ffc
mtfishman <mfishman@flatironinstitute.org>
2026-03-04 09:50:56 -0500

    Revert runtests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,13 @@
-using ITensorPkgSkeleton: ITensorPkgSkeleton
+using ITensors: ITensors
+using Test: @testset
 
-ITensorPkgSkeleton.runtests(; testdir = @__DIR__)
+ITensors.Strided.disable_threads()
+ITensors.BLAS.set_num_threads(1)
+ITensors.disable_threaded_blocksparse()
+
+@testset "$(@__DIR__)" begin
+    dirs = ["ext/ITensorMPSChainRulesCoreExt", "Ops", "base"]
+    for dir in dirs
+        @time include(joinpath(@__DIR__, dir, "runtests.jl"))
+    end
+end
